1e0bf09d6065149d7efa4664ec2595785165584d
AS81 <sanloginchik@gmail.com>
2021-03-12 22:55:23 +0300

    fix: fixed switch/case with range in case

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 module.exports = function toReadable(number) {
-    const upto19 = [
+    const numbersUpto20 = [
         "zero",
         "one",
         "two",
-        "tree",
+        "three",
         "four",
         "five",
         "six",
@@ -33,7 +33,35 @@ module.exports = function toReadable(number) {
     ];
 
     const hundred = "hundred";
+    let humanReadableNumber = "Please, input number in range from 0 up to 999";
 
-    switch (number) {
+    function humanReadableDozens(number) {
+        number % 10 === 0
+            ? (humanReadableNumber = dozens[number / 10])
+            : (humanReadableNumber =
+                  dozens[number / 10] + " " + numbersUpto20[number % 10]);
     }
+    console.log(`Before switch`);
+    switch (true) {
+        case 0 <= number <= 19: //switch(number) -> case 0-19: --- does not work...
+            console.log(`start switch`);
+            humanReadableNumber = numbersUpto20[number];
+            break;
+        case 20 <= number <= 99:
+            humanReadableDozens(number);
+            break;
+        case 100 <= number <= 999:
+            console.log(`start switch`);
+            number % 100 === 0
+                ? (humanReadableNumber = dozens[number / 100] + " " + hundred)
+                : (humanReadableNumber =
+                      dozens[number / 100] +
+                      " " +
+                      hundred +
+                      " " +
+                      humanReadableDozens(number % 100));
+            console.log(`nRN-1: ${humanReadableNumber}`);
+    }
+    console.log(`nRN: ${humanReadableNumber}`);
+    return humanReadableNumber;
 };
