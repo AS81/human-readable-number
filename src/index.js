248accef74f2346c508246385402d9f61a20f4bd
AS81 <sanloginchik@gmail.com>
2021-03-12 21:46:08 +0300

    init: start task, create arrays

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    const upto19 = [
+        "zero",
+        "one",
+        "two",
+        "tree",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+    const dozens = [
+        "twenty",
+        "thirty",
+        "fourty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+
+    const hundred = "hundred";
+
+    switch (number) {
+    }
+};
